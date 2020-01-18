26badc1078029b9f55de4f82cc25e9bfd611aa94
RemyMach <rmachavoine@wynd.eu>
2020-01-18 12:35:40 +0100

    personnalsation de la navigation Bar

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -3,7 +3,6 @@ import { StyleSheet, Image} from 'react-native'
 import { createStackNavigator} from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
-
 import Favorites from '../Components/Favorites'
 import HomePage from '../Components/HomePage'
 
@@ -13,9 +12,6 @@ const SearchStackNavigator = createStackNavigator({
         navigationOptions: {
             title: 'Home'
         }
-    },
-    HomePage: {
-        screen: HomePage
     }
 });
 
@@ -23,17 +19,51 @@ const FavoritesStackNavigator = createStackNavigator({
     Favorites: {
         screen: Favorites,
         navigationOptions: {
-            title: 'FavoriteFilms'
+            title: 'favorites Announces'
         }
     }
 })
 
-const AppTabNavigator = createBottomTabNavigator({
-    HomePage: {
-        screen: SearchStackNavigator
+
+const AppTabNavigator = createBottomTabNavigator(
+    {
+        Home: {
+            screen: SearchStackNavigator,
+            navigationOptions: {
+                tabBarIcon: () => {
+                    return <Image
+                        source={require('../Images/ic_search.png')}
+                        style={styles.icon}
+                    />
+                }
+            }
+        },
+        Favorites: {
+            screen: FavoritesStackNavigator,
+            navigationOptions: {
+                tabBarIcon: () => {
+                    return <Image
+                        source={require('../Images/ic_favorite.png')}
+                        style={styles.icon}
+                    />
+                }
+            }
+        }
     },
-    Favorites: {
-        screen: FavoritesStackNavigator
+    {
+        tabBarOptions: {
+            activeBackgroundColor: '#DDDDDD',//couleur d'arrière-plan de l'onglet sélectionné
+            inactiveBackgroundColor: '#FFFFFF',//couleur d'arrière-plan des onglets non sélectionnés
+            showLabel: true,//on masque les titres
+            showIcon: true // on informe le TabNavigator qu'on souhaite afficher les icônes définis
+        }
+    }
+)
+
+const styles = StyleSheet.create({
+    icon: {
+        width: 30,
+        height: 30
     }
 })
 
